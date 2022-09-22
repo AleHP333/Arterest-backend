d04ecc24b4f0dad16ecdedc160b9f038f0bd6287
AleHP333 <pppppale@gmail.com>
2022-09-22 10:23:39 -0300

    role, isBanned, cart, buyHistory properties added to user model

--- src/models/user.js
+++ src/models/user.js
@@ -3,13 +3,18 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     email: {type: String, required: true},
     userName: {type: String, required: true},
+    userImage: {type: String, required: false},
     password: {type: String, required: true},
     names: {type: String, required: false},
     surnames: {type: String, required: false},
     country: {type: String, required: false},
     city: {type: String, required: false},
     verification: {type: Boolean, required: false},
-    uniqueString: {type: String, required: false}
+    uniqueString: {type: String, required: false},
+    role: {type: String, required: true},
+    isBanned: {type: Boolean, required: false, default: false},
+    cart: {type: mongoose.Types.ObjectId, ref: "products" },
+    buyHistory: {type: mongoose.Types.ObjectId, ref: "products" },
 })
 
 const User = mongoose.model("users", userSchema);
