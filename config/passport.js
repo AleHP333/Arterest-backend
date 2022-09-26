8bf834e789f8a00623d1b95998fdd84fe9bdca53
AleHP333 <pppppale@gmail.com>
2022-09-26 19:49:00 -0300

    likes routes done, SignUp: Now, names allways are unique on register, route generates a random number for the userName, GOOGLE SignUp: Now it receives the imgProp

--- config/passport.js
+++ config/passport.js
@@ -12,6 +12,7 @@ passport.use(
             secretOrKey: SECRET_KEY
         },
         async (jwt_payload, done) => {
+            
             User.findOne({_id: jwt_payload._id})
                 .then((user) => {
                     if(user){
