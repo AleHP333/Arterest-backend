b376f75b4ccfc211a007e648a23f2d76355ad3f6
Anibal Alvarez <alvarezanbl@gmail.com>
2022-09-28 13:41:43 -0300

    autocomplete searchbar

--- src/routes/route/productRoutes.js
+++ src/routes/route/productRoutes.js
@@ -39,13 +39,46 @@ router.get("/allpaints", async (req, res) => {
             return res.status(200).json(products)
         }
         let products = await Product.find()
-
         return res.status(200).json(products)
     } catch (error) {
         console.log(error)
         res.status(500).json({msg: "Internal server error"})
     }
 })
+
+router.get("/autocomplete", async (req, res) => {
+    const {name, art} = req.query
+    try {
+        if(name){
+            let products = await Product.find({userName: {$regex: '.*' + name + '.*', $options: "i"}})
+
+            return res.status(200).json(products)
+        }
+        if(art){
+            let productsUserName = await Product.find({userName: {$regex: '.*' + art + '.*', $options: "i"}})
+            let productsTitle = await Product.find({title: {$regex: '.*' + art + '.*', $options: "i"}})
+            console.log(productsTitle)
+            let productsOrigin = await Product.find({origin: {$regex: '.*' + art + '.*', $options: "i"}})
+            let productsStyle = await Product.find({style: {$regex: '.*' + art + '.*', $options: "i"}})
+            //let productsColors = await ProductTest.find({})
+            let productsTags = await Product.find({tags: {$regex: '.*' + art + '.*', $options: "i"} })
+            let productsColors = await Product.find({colors: {$regex: '.*' + art + '.*', $options: "i"} })
+            let productsTechnique = await Product.find({technique: {$regex: '.*' + art + '.*', $options: "i"} })
+            console.log(productsTags)
+            let productsToMap = [...productsUserName, ...productsTitle, ...productsOrigin, ...productsStyle, ...productsTags, ...productsColors, ...productsTechnique]
+            let products = await [...new Map(productsToMap.map((paint) => [paint["id"], paint])).values()]
+            let sorted = products.sort((a, b) => a.likes.length - b.likes.length).reverse()
+            let autocompleted = sorted.slice(0, 5)
+            return res.status(200).json(autocompleted)
+        }
+        let autocompleted = await Product.find()
+        return res.status(200).json(autocompleted)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({msg: "Internal server error"})
+    }
+})
+
 
 router.get("/getFiveRandom", async (req, res) => {
     try {
