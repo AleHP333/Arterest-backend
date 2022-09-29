646850d332077d748709681426521b744ba020be
Carlos7847 <carlitosmp1597g@gmail.com>
2022-09-28 23:17:38 -0500

    se agrega ruta de allOrders

--- src/routes/route/adminRoutes.js
+++ src/routes/route/adminRoutes.js
@@ -2,6 +2,7 @@ const passport = require("../../../config/passport.js");
 const { Router } = require("express");
 const User = require("../../models/user.js");
 const Product = require("../../models/product.js");
+const Transaction = require("../../models/Transaction");
 const router = Router();
 
 //RECIVE LA ID DEL USUARIO Y UN BOOLEANO (TRUE: BANEADO, FALSE: NO BANEADO XD)
@@ -11,22 +12,32 @@ router
   .put(passport.authenticate("jwt", { session: false }), async (req, res) => {
     const { _id, isBanned } = req.body;
     try {
-        const findUser = await User.findOne({ _id: req.body._id })
-        if(!findUser){return res.status(404).json({msgData:{status: "error", msg: "User not found"}})}
-        if(isBanned === true){
-            findUser.isBanned = isBanned
-            await findUser.save()
-            return res.status(200).json({msgData: {status: "success", msg: "The user was banned"}})
-        } else {
-            findUser.isBanned = isBanned
-            await findUser.save()
-            return res.status(200).json({msgData: {status: "success", msg: "The user was unbanned"}})
-        }
+      const findUser = await User.findOne({ _id: req.body._id });
+      if (!findUser) {
+        return res
+          .status(404)
+          .json({ msgData: { status: "error", msg: "User not found" } });
+      }
+      if (isBanned === true) {
+        findUser.isBanned = isBanned;
+        await findUser.save();
+        return res
+          .status(200)
+          .json({ msgData: { status: "success", msg: "The user was banned" } });
+      } else {
+        findUser.isBanned = isBanned;
+        await findUser.save();
+        return res.status(200).json({
+          msgData: { status: "success", msg: "The user was unbanned" },
+        });
+      }
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({msgData:{ status: "error", msg: "Internal server Error"}})    
+      console.log(error);
+      return res
+        .status(500)
+        .json({ msgData: { status: "error", msg: "Internal server Error" } });
     }
-});
+  });
 
 router
   .route("/modifyProduct")
@@ -71,9 +82,13 @@ router
         },
         { new: true }
       );
-        return res.status(201).json({ msgData: { status: "success", msg: "Product modified successfully"}})
+      return res.status(201).json({
+        msgData: { status: "success", msg: "Product modified successfully" },
+      });
     } catch (error) {
-        return res.status(500).json({msgData:{ status: "error", msg: "Something is wrong"}})
+      return res
+        .status(500)
+        .json({ msgData: { status: "error", msg: "Something is wrong" } });
     }
   });
 
@@ -81,12 +96,28 @@ router
   .route("/getAllUsers")
   .get(passport.authenticate("jwt", { session: false }), async (req, res) => {
     try {
-        let allUsers = await User.find();
-        return res.status(200).json(allUsers);
+      let allUsers = await User.find();
+      return res.status(200).json(allUsers);
     } catch (error) {
-        console.log(error, "getAllUserserror");
-        return res.status(500).json({msgData:{ status: "error", msg: "Something is wrong"}});
+      console.log(error, "getAllUserserror");
+      return res
+        .status(500)
+        .json({ msgData: { status: "error", msg: "Something is wrong" } });
     }
   });
 
+router
+  .route("/getAllOrders")
+  .get(passport.authenticate("jwt", { session: false }), async (req, res) => {
+    try {
+      let orders = await Transaction.find();
+      console.log(orders, "transaction");
+      return res.status(200).json(orders);
+    } catch (error) {
+      console.log(error, "getAllOrdersError");
+      return res
+        .status(500)
+        .json({ msgData: { status: "error", msg: "Something is wrong" } });
+    }
+  });
 module.exports = router;
